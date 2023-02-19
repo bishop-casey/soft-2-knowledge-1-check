3aa1d0192137fa65f7c66e2bfa4b27458ee42b9d
bishop-casey <112920878+bishop-casey@users.noreply.github.com>
2023-02-19 08:44:55 -0500

    Add files via upload
    Created Construction Class and ConstructAptHR Class

---
+++ Software2KnowledgeCheck1/Software2KnowledgeCheck1/ConstructAptHR.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software2KnowledgeCheck1
+{
+    internal class ConstructAptHR : Construction
+    {
+        public List<Building> Buildings { get; } = new List<Building>();
+
+        public void CreateApartment(Apartment apartment, HighRise highRise)
+        {
+            // Get materials
+            var materialRepo = new MaterialsRepo();
+            var materialsNeeded = materialRepo.GetMaterials();
+
+            var permitRepo = new ZoningAndPermitRepo();
+
+            var apartmentWasMade = ConstructBuilding<Apartment>(materialsNeeded, permitRepo.GetPermit(), permitRepo.ZoningApproves());
+            var highRiseWasMade = ConstructBuilding<HighRise>(materialsNeeded, permitRepo.GetPermit(), permitRepo.ZoningApproves());
+
+            if (apartmentWasMade)
+            {
+                Buildings.Add(apartment);
+            }
+            if (highRiseWasMade)
+            {
+                Buildings.Add(highRise);
+            }
+        }
+    }
+}
---
+++ Software2KnowledgeCheck1/Software2KnowledgeCheck1/Construction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software2KnowledgeCheck1
+{
+    internal class Construction
+    {
+        public bool ConstructBuilding<T>(List<string> materials, bool permit, bool zoning) where T : Building
+        {
+            if (permit && zoning)
+            {
+                foreach (var material in materials)
+                {
+                    if (material == "concrete")
+                    {
+                        // start laying foundation
+                    }
+                    else if (material == "Steel")
+                    {
+                        // Start building structure
+                    }
+                    // etc etc...
+
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
